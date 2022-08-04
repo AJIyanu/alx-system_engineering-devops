e8666ff19cd231538625225514ce36787d0c91e3
AJIyanu <aderemijoseph@live.com>
2022-08-04 11:39:49 -0700

    I sabi

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 from =  ARGV[0].scan(/(?<=from:).?\w+/).join
-to = ARGV[0].scan(/?<=to:).?\w+/).join
+to = ARGV[0].scan(/(?<=to:).?\w+/).join
 flag = ARGV[0].scan(/(?<=flags:).{1,12}/).join
 puts "#{from},#{to},#{flag}"
