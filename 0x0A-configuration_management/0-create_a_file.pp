e24275477d6e06fb2dec2e7729028579b8881b90
AJIyanu <aderemijoseph@live.com>
2022-09-16 16:02:27 -0700

    not too much wahala i guess

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,15 +1,10 @@
 # Now create school under /tmp/
 
-class file_creator {
-
   file { '/tmp/school':
 
-    ensure => 'present',
+    ensure  => 'present',
     content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
-    
   }
-
-}
