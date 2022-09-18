ccd0c7289cd6d5a2811d36fdf4809ac1e9eaa44c
AJIyanu <aderemijoseph@live.com>
2022-09-18 15:34:21 -0700

    lemme sleep

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# using puppet to make changes to an SSH config file
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
