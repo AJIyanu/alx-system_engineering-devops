7ed7995d567fe6784e3b5ac66b2da8ee6c7fe867
AJIyanu <aderemijoseph@live.com>
2022-11-03 03:03:15 +0100

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # that is an error in some cconfiguration we need to correcct here
 
 exec { "change wp-locale extension":
-    command => 'sed  -i  s/phpp/php/g /var/www/html/wp-settings.php',
+    command  => 'sed  -i  s/phpp/php/g /var/www/html/wp-settings.php',
     provider => shell,
 }
