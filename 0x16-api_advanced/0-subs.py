f00b82f44aa09ab7d97f1cddae01221928324f3c
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-06-06 17:11:08 +0100

    finish up

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ a function that queries the Reddit API and returns
+ the number of subscribers (not active users,
+ total subscribers) for a given subreddit.
+ If an invalid subreddit is given, the function should return 0.
+ """
+
+
+import requests
+import json
+
+def number_of_subscribers(subreddit):
+    """
+    returns numbers of subscriber
+    if subreddit not valid returns 0
+    """
+    headers = {"User-Agent": "AJ Iyanu"}
+    url = f"http://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return 0
+    users = json.loads(response.text)
+    return users.get("data").get('subscribers') if not None else 0
