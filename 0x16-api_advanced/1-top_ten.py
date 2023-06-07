fb42cd6aeec01139a9fa1bcdb932f1b729f0c74b
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-06-07 11:26:21 +0100

    print 10 hot titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,27 +4,19 @@ a function that queries the Reddit API and
 prints the titles of the first 10 hot posts
 listed for a given subreddit.
 """
-try:
-    import requests
-except ImportError:
-    import collections.abc
-    collections.Mapping = collections.abc.Mapping
-    collections.MutableMapping = collections.abc.MutableMapping
-    collections.Iterable = collections.abc.Iterable
-    collections.MutableSet = collections.abc.MutableSet
-    collections.Callable = collections.abc.Callable
-    import requests
+import requests
 
 
 def top_ten(subreddit):
     """prints the first 10 hot posts"""
     headers = {"User-Agent": "AJ Iyanu"}
-    url = f"http://www.reddit.com/r/{subreddit}/hot.json"
+    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=True)
     title = response.json()
     if title.get("data").get("dist") == 0:
         print("None")
         return
+    count = 1
     for data in title["data"]["children"]:
         print(data["data"]["title"])
