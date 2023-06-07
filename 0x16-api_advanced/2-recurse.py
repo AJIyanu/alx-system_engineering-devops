322e5632274d97be10a4978553f32de67887d6a3
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-06-07 12:56:39 +0100

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+queries the Reddit API and returns a list
+containing the titles of all hot articles
+for a given subreddit. If no results are
+found for the given subreddit, the function
+should return None.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[0]):
+    """returns all titles and None for invalid"""
+    headers = {"User-Agent": "AJ Iyanu"}
+    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"after": hot_list[0]}
+    if hot_list[0] != 0:
+        response = requests.get(url, headers=headers, params=params)
+    else:
+        response = requests.get(url, headers=headers)
+    title = response.json()
+    if title.get("data").get("dist") == 0:
+        print("None")
+        return
+    hot_list[0] = title["data"]["after"]
+    if hot_list[0] is None:
+        for data in title["data"]["children"]:
+            hot_list.append(data["data"]["title"])
+        return hot_list
+    else:
+        for data in title["data"]["children"]:
+            hot_list.append(data["data"]["title"])
+    return recurse(subreddit, hot_list)
